6eae441f22d8ff30310b6ba33b430dd4f92cd53f
佛系阿宅 <x3374918255@gmail.com>
2026-08-02 01:00:41 +0800

    背景

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -150,7 +150,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 壁纸透明度
 		opacity: 0.8,
 		// 背景模糊度
-		blur: 10,
+		blur:0,
 		// 卡片透明度，0-1之间，值越小越透明
 		cardOpacity: 0.5,
 	},
